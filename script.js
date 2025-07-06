0ef853cb6c5a960fcf40b6982fe30b7310dfb9f7
Hoax <149868997+HoaxAA@users.noreply.github.com>
2025-07-06 15:31:34 +0330

    Skeleton of game
    created a simple index.html
    almost all of the game logic (one is missing)

---
+++ script.js
@@ -0,0 +1,72 @@
+function computerChoice() {
+    let res = Math.floor(Math.random() * 3);
+    switch(res) {
+    case 0:
+        res = 'rock';
+        break;
+    case 1:
+        res = 'paper';
+        break;
+    case 2:
+        res = 'scissor';
+        break;    
+    }
+    return res;
+}
+
+function humanChoice() {
+    choice = prompt();
+    choice = String(choice).toLowerCase();
+    return choice;
+}
+
+function playRound(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice) {
+        console.log("Computer chose: ", computerChoice, "\nDraw");
+        console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+    }
+    else if (humanChoice == 'rock') {
+        if (computerChoice == 'paper') {
+            computerScore++;
+            console.log("Computer chose: paper\nYou lost this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+        else if (computerChoice == 'scissor') {
+            humanScore++;
+            console.log("Computer chose: scissor\nYou won this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+    }
+    else if (humanChoice == 'paper') {
+        if (computerChoice == 'rock') {
+            humanScore++;
+            console.log("Computer chose: rock\nYou won this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+        else if (computerChoice == 'scissor') {
+            computerScore++;
+            console.log("Computer chose: scissor\nYou lost this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+    }
+    else if (humanChoice == 'scissor') {
+        if (computerChoice == 'rock') {
+            computerScore++;
+            console.log("Computer chose: rock\nYou lost this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+        else if (computerChoice == 'paper') {
+            humanScore++;
+            console.log("Compute chose: paper\nYou won this round");
+            console.log("Your score: ", humanScore, " Computer Score: ", computerScore);
+        }
+    }
+}
+
+let humanScore = 0, computerScore = 0;
+
+for (i = 0; i < 5; ++i) {
+    // let humanSelection = humanChoice();
+    // let computerSelection = computerChoice();
+    playRound(humanChoice(), computerChoice());
+}
